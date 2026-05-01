d7b8c0ba39a24475a795fe554865b31fbfc41ff9
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2026-05-01 18:53:30 +0000

    Deploying backend

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -32,8 +32,8 @@ const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,    
-            sameSite: "lax", 
+            secure: true,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         });
         
